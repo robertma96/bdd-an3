d975c278dff8baa70cacbf6065d1581c39f75409
robertma96 <37088728+robertma96@users.noreply.github.com>
2019-08-06 14:49:41 +0300

    Update settings.py

--- sitespital/sitespital/settings.py
+++ sitespital/sitespital/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'spital',
         'USER': 'root',
-        'PASSWORD': 'masina',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
